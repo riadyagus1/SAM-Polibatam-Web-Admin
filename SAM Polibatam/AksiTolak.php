2977d15c96390ffb8992c394ef50ed7e34b7ce78
HadesShade <54205634+HadesShade@users.noreply.github.com>
2021-04-29 14:44:04 +0700

    Update AksiTolak.php
    Close connection after process finished.

--- SAM Polibatam/AksiTolak.php
+++ SAM Polibatam/AksiTolak.php
@@ -1,11 +1,15 @@
 <?php
-
-	<?php
 	include 'koneksi.php';
 	$id = $_GET['id_header'];
 	$query="DELETE from tbl_izin WHERE id_header='$id';";
-	mysqli_query($koneksi, $query);
 
-	header("location:ApprovalIzin.php")
-?>
+	if ($koneksi->query($query) === TRUE) {
+		$koneksi->close();
+		header("location:ApprovalIzin.php")
+	}
+
+	else {
+		$koneksi->close();
+		echo "Error!";
+	}
 ?>
