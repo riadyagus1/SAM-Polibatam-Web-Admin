a4a91b00c094001ecbedc75c6889529e3b0702f1
HadesShade <54205634+HadesShade@users.noreply.github.com>
2021-04-29 14:40:13 +0700

    Update Aksi.php
    Update error on Aksi.php

--- SAM Polibatam/Aksi.php
+++ SAM Polibatam/Aksi.php
@@ -1,10 +1,17 @@
-<?php   
+<?php
 
-if (isset($_GET['IsAccepted'], $_GET['id_header'])) {
-    $stmt = mysqli_prepare($conn, "UPDATE tbl_izin SET IsAccepted = 1 WHERE id_header= ?");
-    mysqli_stmt_bind_param($stmt, "sd", $_GET['IsAccepted'], $_GET['id_header']);
+include 'koneksi.php';   
+
+if ($_GET['id_header'])) {
+    $stmt = $koneksi->prepare("UPDATE tbl_izin SET IsAccepted = 1 WHERE id_header= ?");
+
+    $stmt->bind_param("s", $id);
+    
+    $id = $_GET['id_header'];
+    
     $stmt->execute();
     $stmt->close();
+    $koneksi->close();
     }
 
 ?>
